c31aea42757c5d84459121b5bf95ea50dc11115c
jgabogado2 <119831161+jgabogado2@users.noreply.github.com>
2023-09-12 18:11:54 +0800

    Add files via upload

---
+++ ENTJAVA_Week3/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using ENTJAVA_Week3.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using ENTJAVA_Week3.Models.EntityManager;
+
+
+namespace ENTJAVA_Week3.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                if (!um.IsLoginNameExist(user.LoginName))
+                {
+                    um.AddUserAccount(user);
+                    // FormsAuthentication.SetAuthCookie(user.FirstName, false);
+                    return RedirectToAction("", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            var users = new UserManager().GetAllUsers();
+
+            return View();
+        }
+    }
+}
