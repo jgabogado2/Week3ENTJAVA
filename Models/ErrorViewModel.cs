e62713bf419dec026792257109db18da7b5c6bb3
jgabogado2 <119831161+jgabogado2@users.noreply.github.com>
2023-09-20 23:03:09 +0800

    Updates

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ENTJAVA_Week3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
